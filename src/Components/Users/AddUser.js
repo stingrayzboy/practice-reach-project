6c3c15b19ef0fd9ac24cd618fecf7cc055d3980b
Faraz Noor <faraznoor75@gmail.com>
2022-08-16 22:22:12 +0530

    Using Use state to have more refined control

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
-import Card from "../UI/Card"
-import classes from './AddUser.module.css'
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 const AddUser = (props) => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const getAge = (event) => {
+    setAge(event.target.value);
+  };
+
+  const getName = (event) => {
+    setName(event.target.value);
+  };
+
   const formHandler = (event) => {
     event.preventDefault();
     const data = {
-      name: event.target.username.value,
-      age: event.target.userage.value,
+      name: name,
+      age: age,
     };
     props.addUser(data);
+    setName("");
+    setAge("");
   };
 
   return (
-    <Card className={classes.input} >  
-    <form onSubmit={formHandler}>
-      <label htmlFor="userName">User Name</label>
-      <input type="text" id="username" placeholder="Your Name" />
-      <label htmlFor="userAge">User Age</label>
-      <input type="number" id="userage" placeholder="Your Age" />
-      <Button type="submit"> Add User</Button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={formHandler}>
+        <label htmlFor="userName">User Name</label>
+        <input
+          type="text"
+          id="username"
+          placeholder="Your Name"
+          value={name}
+          onChange={getName}
+        />
+        <label htmlFor="userAge">User Age</label>
+        <input
+          type="number"
+          id="userage"
+          placeholder="Your Age"
+          value={age}
+          onChange={getAge}
+        />
+        <Button type="submit"> Add User</Button>
+      </form>
     </Card>
   );
 };
